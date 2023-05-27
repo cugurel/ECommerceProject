854dde3c4965d1b118bb7a30a7a24f2294d9b8ad
Cugurel <cugurel7@gmail.com>
2023-05-27 16:14:56 +0300

    Project Updated

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,5 @@
 ﻿using Entity.Abstract;
-using Entity.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
